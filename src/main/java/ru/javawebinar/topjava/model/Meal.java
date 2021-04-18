da448a4e6fd5026ee97cbdf695754634c305099d
anton <bio@forever-ds.com>
2021-04-18 21:41:47 +0300

    Migrate to JDK-15 jakarta removed Meal JPA-repository implemented with hibernate

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,17 +1,42 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import com.sun.istack.NotNull;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(name = "meals_unique_user_datetime_idx", columnNames = {"user_id", "date_time"})})
+@NamedQueries({
+		@NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id =:userId ORDER BY m.dateTime DESC"),
+		@NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id =:userId AND m.dateTime >= :start AND m.dateTime < :end  ORDER BY m.dateTime DESC"),
+		@NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.user.id =:userId AND m.id =:id")
+})
 public class Meal extends AbstractBaseEntity {
+	public static final String ALL_SORTED = "Meal.getAll";
+	public static final String GET_BETWEEN = "Meal.getBetween";
+	public static final String DELETE = "Meal.delete";
+
+	@Column(name = "date_time", nullable = false, columnDefinition = "timestamp default now()")
+	@NotNull
     private LocalDateTime dateTime;
+
+	@Column(name = "description", nullable = false)
+	@NotBlank
+	@Size(max=120)
     private String description;
+
+	@Column(name = "calories", nullable = false)
+	@Range(min=0, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Meal() {
