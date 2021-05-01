0d545a1e8c977f6d690e6c8893f069064d6a52b5
anton <bio@forever-ds.com>
2021-05-01 22:06:48 +0300

    Migrate to JDK-15 jakarta removed Meal JPA-repository implemented with hibernate

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -23,7 +23,7 @@ public class InMemoryAdminRestControllerSpringTest {
     private InMemoryUserRepository repository;
 
     @Before
-    public void setUp() {
+    public void setup() {
         repository.init();
     }
 
